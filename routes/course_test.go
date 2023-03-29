a8d61a577db3d26073cccb3bc8e259dc77ec6940
szewczukk <dmitro.szewczuk@gmail.com>
2023-03-29 14:50:07 +0200

    Add TestExistingDetailedCourse test

--- routes/course_test.go
+++ routes/course_test.go
@@ -1,6 +1,7 @@
 package routes_test
 
 import (
+	"errors"
 	"html/template"
 	"net/http"
 	"net/http/httptest"
@@ -14,15 +15,25 @@ import (
 
 type MockHomePageStorage struct {
 	courses []*types.Course
-	nextId  int
+	nextId  int64
 }
 
 func (s *MockHomePageStorage) GetAllCourses() ([]*types.Course, error) {
 	return s.courses, nil
 }
 
+func (s *MockHomePageStorage) GetCourse(id int64) (*types.Course, error) {
+	for _, course := range s.courses {
+		if course.Id == int(id) {
+			return course, nil
+		}
+	}
+
+	return nil, errors.New("Not found")
+}
+
 func (s *MockHomePageStorage) CreateCourse(name string) error {
-	s.courses = append(s.courses, &types.Course{Id: s.nextId, Name: name})
+	s.courses = append(s.courses, &types.Course{Id: int(s.nextId), Name: name})
 	s.nextId++
 	return nil
 }
@@ -63,6 +74,25 @@ func TestHomePagePOST(t *testing.T) {
 
 	result := w.Body.String()
 	if result != "0 German 1 Spanish " {
+		t.Errorf("Wrong body returned %v", result)
+	}
+}
+
+func TestExistingDetailedCourse(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/course?id=0", nil)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+
+	storage := &MockHomePageStorage{
+		nextId:  1,
+		courses: []*types.Course{{Id: 0, Name: "German"}},
+	}
+	template, _ := template.New("homepage").Parse("{{.Id}} {{.Name}}")
+
+	routes.DetailedCourse(storage, template)(w, req)
+
+	result := w.Body.String()
+	if result != "0 German" {
 		t.Errorf("Wrong body returned %v", result)
 	}
 }
