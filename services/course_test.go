84fbc12d6ef9e944f0e5353e52eddb440073b200
szewczukk <dmitro.szewczuk@gmail.com>
2023-04-27 14:25:59 +0200

    Add more test for updating course

--- services/course_test.go
+++ services/course_test.go
@@ -309,15 +309,42 @@ func TestUpdateCouseGetNotFound(t *testing.T) {
 	}
 }
 
+func TestUpdateCouseGetInvalidId(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/update-course?id=a", nil)
+	w := httptest.NewRecorder()
+
+	storage := storage.NewMemoryStorage([]*types.Course{})
+	temp, _ := template.New("homepage").Parse("{{.Id}} {{.Name}}")
+	templates := map[string]*template.Template{
+		"DetailedCourse": temp,
+	}
+	service := services.NewCoursesService(storage, templates)
+
+	service.UpdateCourseGET(w, req)
+
+	body := w.Body.String()
+	statusCode := w.Result().StatusCode
+
+	if body != "strconv.ParseInt: parsing \"a\": invalid syntax\n" {
+		t.Errorf("Wrong body, got: %v", body)
+	}
+
+	if statusCode != 400 {
+		t.Errorf("Wrong status code, got: %v", statusCode)
+	}
+}
+
 func TestUpdateCoursePost(t *testing.T) {
 	form := url.Values{}
+	form.Add("id", "0")
 	form.Add("name", "Spanish")
 
 	req := httptest.NewRequest(
 		http.MethodPost,
-		"/update-course?id=0",
+		"/update-course",
 		strings.NewReader(form.Encode()),
 	)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	w := httptest.NewRecorder()
 
 	storage := storage.NewMemoryStorage([]*types.Course{{Id: 0, Name: "German"}})
@@ -327,18 +354,122 @@ func TestUpdateCoursePost(t *testing.T) {
 	service.UpdateCoursePOST(w, req)
 
 	statusCode := w.Result().StatusCode
-	url, _ := w.Result().Location()
-	id := url.Query().Get("id")
-
 	if statusCode != 301 {
 		t.Errorf("Wrong status code returned, got: %v", statusCode)
 	}
 
+	course, err := storage.GetCourse(0)
+	if err != nil {
+		t.Errorf("Error occurred, got: %v", course.Name)
+	}
+
+	if course.Name != "Spanish" {
+		t.Errorf("Wrong name, got: %v", course.Name)
+	}
+
+	url, _ := w.Result().Location()
 	if url.Path != "/courses" {
 		t.Errorf("Wrong redirection url, got: %v", url.Path)
 	}
 
+	id := url.Query().Get("id")
 	if id != "0" {
 		t.Errorf("Wrong id in the query, got: %v", id)
+	}
+}
+
+func TestUpdateCoursePostInvalidId(t *testing.T) {
+	form := url.Values{}
+	form.Add("id", "a")
+	form.Add("name", "Spanish")
+
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/update-course",
+		strings.NewReader(form.Encode()),
+	)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+
+	storage := storage.NewMemoryStorage([]*types.Course{{Id: 0, Name: "German"}})
+	templates := map[string]*template.Template{}
+	service := services.NewCoursesService(storage, templates)
+
+	service.UpdateCoursePOST(w, req)
+
+	body := w.Body.String()
+	statusCode := w.Result().StatusCode
+	url, _ := w.Result().Location()
+
+	if statusCode != 400 {
+		t.Errorf("Wrong status code returned, got: %v", statusCode)
+	}
+
+	if body != "strconv.ParseInt: parsing \"a\": invalid syntax\n" {
+		t.Errorf("Wrong redirection url, got: %v", url.Path)
+	}
+}
+
+func TestUpdateCoursePostNotFound(t *testing.T) {
+	form := url.Values{}
+	form.Add("id", "0")
+	form.Add("name", "Spanish")
+
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/update-course",
+		strings.NewReader(form.Encode()),
+	)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+
+	storage := storage.NewMemoryStorage([]*types.Course{})
+	templates := map[string]*template.Template{}
+	service := services.NewCoursesService(storage, templates)
+
+	service.UpdateCoursePOST(w, req)
+
+	body := w.Body.String()
+	statusCode := w.Result().StatusCode
+	url, _ := w.Result().Location()
+
+	if statusCode != 404 {
+		t.Errorf("Wrong status code returned, got: %v", statusCode)
+	}
+
+	if body != "not found\n" {
+		t.Errorf("Wrong redirection url, got: %v", url.Path)
+	}
+}
+
+func TestUpdateCoursePostDuplicate(t *testing.T) {
+	form := url.Values{}
+	form.Add("id", "1")
+	form.Add("name", "Spanish")
+
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/update-course",
+		strings.NewReader(form.Encode()),
+	)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+
+	storage := storage.NewMemoryStorage([]*types.Course{{Id: 0, Name: "Spanish"}, {Id: 1, Name: "German"}})
+	templates := map[string]*template.Template{}
+	service := services.NewCoursesService(storage, templates)
+
+	service.UpdateCoursePOST(w, req)
+
+	body := w.Body.String()
+	statusCode := w.Result().StatusCode
+	url, _ := w.Result().Location()
+
+	if statusCode != 404 {
+		t.Errorf("Wrong status code returned, got: %v", statusCode)
+	}
+
+	if body != "duplicate\n" {
+		t.Errorf("Wrong redirection url, got: %v", url.Path)
 	}
 }
